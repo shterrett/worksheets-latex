30fdef3b7ad5c24d01cfa40b2007d25b8df9fadb
Stuart Terrett <shterrett@gmail.com>
2013-03-15 07:06:47 -0400

    Latex helper filled out; not tested yet

--- generators/latex_helper.rb
+++ generators/latex_helper.rb
@@ -1,3 +1,31 @@
 module LatexHelper
 
+  def make_fraction(num, denom)
+    "\\frac{#{num}}{#{denom}}"
+  end
+
+  def addition(first, second)
+    "#{first} + #{second}"
+  end
+  
+  def subtraction(first, second)
+    "#{first} - #{second}"
+  end
+  
+  def multiplication(first, second)
+    "#{first} \\cdot #{second}"
+  end
+  
+  def division(first, second)
+    "#{first} \\div #{second}"
+  end
+  
+  def inline_math_mode(str)
+    "\\[#{str}\\]"
+  end
+  
+  def answer_space(str)
+    "\\makebox{\\begin{minipage} {0.45\\textwidth} #{str} \\hfill \\vspace{4cm} \\end{minipage}}"
+  end
+  
 end
